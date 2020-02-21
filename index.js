188ebffc2c9a388d491ed4d62859820cf86722d7
Alejandra Angulo Rico <43595879+Alangulo@users.noreply.github.com>
2020-02-20 22:29:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ const port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+var theThing = null;
+var replaceThing = function () {
+  var priorThing = theThing;  // hold on to the prior thing
+  var unused = function () {
+    // 'unused' is the only place where 'priorThing' is referenced,
+    // but 'unused' never gets invoked
+    if (priorThing) {
+      console.log("hi");
+    }
+  };
+  theThing = {
+    longStr: new Array(1000000).join('*'),  // create a 1MB object
+    someMethod: function () {
+      console.log(someMessage);
+    }
+  };
+};
+setInterval(replaceThing, 1000);    // invoke `replaceThing' once every second
