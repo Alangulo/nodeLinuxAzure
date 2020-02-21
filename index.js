0ed94361fe4ceb8a125253dba0a4046d56c88d3d
Alejandra Angulo Rico <43595879+Alangulo@users.noreply.github.com>
2020-02-20 23:26:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,16 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    var theThing = null;
-var replaceThing = function () {
-  var priorThing = theThing;  // hold on to the prior thing
-  var unused = function () {
-    // 'unused' is the only place where 'priorThing' is referenced,
-    // but 'unused' never gets invoked
-    if (priorThing) {
-      console.log("hi");
-    }
-  };
-  theThing = {
-    longStr: new Array(1000000).join('*'),  // create a 1MB object
-    someMethod: function () {
-      console.log(someMessage);
-    }
-  };
-};
+    var decimals = [];
+var max = 20000;
+
+recurse();
+
+function recurse() {
+  decimals.push(Math.floor(Math.random()*10));
+  if(decimals.length === max) return;
+  recurse();
+}
 setInterval(replaceThing, 1000);    // invoke `replaceThing' once every second
     response.end("Hello World!");
 });
